5f6eeefa825766f6381bfda97d0bb6117631115c
Deepak Bajaj <bajajd382@gmail.com>
2025-12-07 17:48:33 +0530

    Fix React reference in App.jsx; add favicon placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchBar from './components/SearchBar'
 import FiltersPanel from './components/FiltersPanel'
 import SortingDropdown from './components/SortingDropdown'
